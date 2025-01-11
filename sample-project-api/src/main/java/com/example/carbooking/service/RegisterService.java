f992d7cd3cad7ed541d2f670984dfbd24cc8812d
Javaspring-dev <jtraining32@gmail.com>
2025-01-11 14:45:52 +0530

    Delete sample-project-api directory

--- sample-project-api/src/main/java/com/example/carbooking/service/RegisterService.java
+++
@@ -1,9 +0,0 @@
-package com.example.carbooking.service;
-
-import com.example.carbooking.dto.LoginDto;
-import com.example.carbooking.entities.RegisterEntity;
-
-public interface RegisterService {
-    RegisterEntity create(RegisterEntity registerEntity);
-    RegisterEntity login(LoginDto loginDto);
-}
